2d45f221a34f5ca880bfbaeeaaddfe33ee12c4a1
saleem <salee@10.220.17.236>
2024-03-18 12:44:54 -0400

    regg

--- ludogame/ludogame.java
+++ ludogame/ludogame.java
@@ -5,7 +5,7 @@ public class ludogame {
 		// TODO Auto-generated method stub
 System.out.print("yo");
 <<<<<<< HEAD
-
+System.out.print("reg");
 =======
 System.out.println("How yall doing");
 >>>>>>> branch 'master' of https://github.com/Carters5wit/CS-Final-Project.git
