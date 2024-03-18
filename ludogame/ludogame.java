eb0b64ec437a7dbb241f23e475814f7edb228762
Karat <Karat@10.220.16.119>
2024-03-18 12:43:41 -0400

    reg

--- ludogame/ludogame.java
+++ ludogame/ludogame.java
@@ -5,7 +5,7 @@ public class ludogame {
 		// TODO Auto-generated method stub
 System.out.print("yo");
 <<<<<<< HEAD
-System.out.print("Ronny sexy");
+
 =======
 System.out.println("How yall doing");
 >>>>>>> branch 'master' of https://github.com/Carters5wit/CS-Final-Project.git
