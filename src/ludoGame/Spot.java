608419fe45e37564133e4939e6a082060744b71c
Reggie Andrade <andrader6@wit.edu>
2024-03-27 12:12:12 -0400

    Added basic spot drawing

--- src/ludoGame/Spot.java
+++ src/ludoGame/Spot.java
@@ -1,40 +1,90 @@
 package ludoGame;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Spot {
-	private String catagory = "Neutral";
+	private String type;
+	private GraphicsContext gc;
 	private static int id = 0;
+    private double x;
+    private double y;
+    private final double size = 60; // Fixed size, do not change
 	
-	public Spot() {
+    // TODO: Document this function
+	public Spot(double x, double y, String type, GraphicsContext gc) {
 		id++;
+		this.x = x;
+        this.y = y;
+        this.type = type;
+        
+        boolean square = true;
+        Color fillColor = Color.WHITE;
+        Color outlineColor = Color.BLACK;
+        
+        switch (type) {
+        	case "neutral":
+        		break;
+        	case "home":
+                double radius = size / 2.0;
+                double centerX = x + radius;
+                double centerY = y + radius;
+
+                // Draw a circular spot
+                square = false;
+                gc.setFill(fillColor);
+                gc.setStroke(outlineColor);
+                gc.fillOval(centerX - radius, centerY - radius, size, size);
+                gc.strokeOval(centerX - radius, centerY - radius, size, size);
+                break;
+        	// TODO: Write the rest of the switch case statements
+        }
+        
+        if (square) {
+        	gc.setFill(fillColor);
+            gc.setStroke(outlineColor);
+            gc.fillRect(x, y, size, size);
+            gc.strokeRect(x, y, size, size);
+        }
 	}
-	public static int getID() {
+	
+	// TODO: Document this function
+	public Spot(double x, double y, GraphicsContext gc) {
+		id++;
+		this.x = x;
+        this.y = y;
+        this.gc = gc;
+        this.type = "neutral";
+        gc.setFill(Color.WHITE);
+        gc.setStroke(Color.BLACK);
+        gc.fillRect(x, y, size, size);
+        gc.strokeRect(x, y, size, size);
+	}
+	
+	// TODO: Document this function
+	public int getID() {
 		return id;
 	}
 	
-	public void setHome() {
-		this.catagory = "Home";
+	// TODO: Document this function
+	public double getX() {
+		return x;
 	}
 	
-	public void setSafe() {
-		this.catagory = "Safe";
+	// TODO: Document this function
+	public double getY() {
+		return y;
 	}
 	
-	public void setStart() {
-		this.catagory = "Start";
-	}
-	
-	public void setHomeStretch() {
-		this.catagory = "HomeStretch";
-	}
-
+	// TODO: Document this function
 	public String getType() {
-		return catagory;
+		return type;
 	}
 	
-
-	
-	
-	
-	
+	// TODO: Document this function
+	public void setType(String type) {
+		// TODO: Set type based on keywords, throwing error for invalid type.
+		// Tip: Use gc.clearRect to replace a shape, use switch cases from constructor.
+	}
 	
 }
