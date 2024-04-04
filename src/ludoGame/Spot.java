0228249e9285c02b4486091af5c68585f0a8608c
saleem <salee@10.220.17.236>
2024-04-03 20:42:23 -0400

    pls

--- src/ludoGame/Spot.java
+++ src/ludoGame/Spot.java
@@ -1,63 +1,89 @@
 package ludoGame;
-
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
-
+import javafx.scene.transform.Rotate;
 public class Spot {
 	private String type;
 	private GraphicsContext gc;
 	private static int id = 0;
-    private double x;
-    private double y;
-    private final double size = 50; // Fixed size, do not change
+   private double x;
+   private double y;
+  
+   private final double size = 50; // Fixed size, do not change
+  
 	
-    // TODO: Document this function
+   // TODO: Document this function
 	public Spot(double x, double y, String type, GraphicsContext gc) {
 		id++;
 		this.x = x;
-        this.y = y;
-        this.type = type;
-        
-        boolean square = true;
-        Color fillColor = Color.WHITE;
-        
-        switch (type) {
-        	case "neutral":
-        		break;
-        	case "home":
-                double radius = size / 2.0;
-                double centerX = x + radius;
-                double centerY = y + radius;
-
-                // Draw a circular spot
-                square = false;
-                gc.setFill(fillColor);
-                gc.setStroke(Color.BLACK);
-                gc.fillOval(centerX - radius, centerY - radius, size, size);
-                gc.strokeOval(centerX - radius, centerY - radius, size, size);
-                break;
-        	// TODO: Write the rest of the switch case statements
-        }
-        
-        if (square) {
-        	gc.setFill(fillColor);
-            gc.setStroke(Color.BLACK);
-            gc.fillRect(x, y, size, size);
-            gc.strokeRect(x, y, size, size);
-        }
+       this.y = y;
+       this.type = type;
+      
+       boolean normal = true;
+       Color fillColor = Color.WHITE;
+      
+       switch (type) {
+       	case "neutral":
+       		break;
+       	case "home":
+               double radius = size / 2.0;
+               double centerX = x + radius;
+               double centerY = y + radius;
+               // Draw a circular spot
+               normal = false;
+               gc.setFill(fillColor);
+               gc.setStroke(Color.BLACK);
+               gc.fillOval(centerX - radius, centerY - radius, size, size);
+               gc.strokeOval(centerX - radius, centerY - radius, size, size);
+               break;
+       	// TODO: Write the rest of the switch case statements
+       	case "start":
+       		
+       		gc.setFill(fillColor);
+               gc.setStroke(Color.BLACK);
+               gc.fillRect(x, y, size, size);
+               gc.strokeRect(x, y, size, size);
+            
+       	
+       }
+      
+       if (normal) {
+       	gc.setFill(fillColor);
+           gc.setStroke(Color.BLACK);
+           gc.fillRect(x, y, size, size);
+           gc.strokeRect(x, y, size, size);
+       }
 	}
 	
 	// TODO: Document this function
 	public Spot(double x, double y, GraphicsContext gc) {
 		id++;
 		this.x = x;
-        this.y = y;
-        this.gc = gc;
-        this.type = "neutral";
-        gc.setFill(Color.WHITE);
-        gc.setStroke(Color.BLACK);
-        gc.fillRect(x, y, size, size);
-        gc.strokeRect(x, y, size, size);
+       this.y = y;
+       this.gc = gc;
+       this.type = "neutral";
+       gc.setFill(Color.WHITE);
+       gc.setStroke(Color.BLACK);
+       gc.fillRect(x, y, size, size);
+       gc.strokeRect(x, y, size, size);
+	}
+	public void addArrow(int angle) {
+		Image img = new Image("src/images/plain-arrow.png");
+       ImageView arrow = new ImageView(img);
+       arrow.setX(x);
+       arrow.setY(y);
+      
+      
+       Rotate r = new Rotate();
+       r.setAngle(angle);
+       r.setPivotX(img.getWidth()/2);
+       r.setPivotY(img.getHeight()/2);
+       arrow.getTransforms().add(r);
+      
+      
+      
 	}
 	
 	// TODO: Document this function
@@ -95,3 +121,4 @@ public class Spot {
 	}
 	
 }
+
