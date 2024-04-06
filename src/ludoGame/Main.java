0c3bbf969db8c53bce31ddb5fb894412ee5c211b
Reggie Andrade <andrader6@wit.edu>
2024-04-06 19:52:15 -0400

    Added basic pawn placement logic

--- src/ludoGame/Main.java
+++ src/ludoGame/Main.java
@@ -19,10 +19,50 @@ public class Main extends Application {
         
         // Create a Board object and add it to the root
         Board board = new Board(750, 750);
+        GraphicsContext gc = board.getGraphicsContext();
         root.getChildren().add(board.getCanvas());
         
         // Set scene size and show to screen
         primaryStage.setScene(new Scene(root, 750, 750));
         primaryStage.show();
+        
+        
+        // Get spots on the board
+        Spot[][] spots = board.getSpots();
+        int[][][] homes = board.getHomes();
+        
+        // Initialize pawn arrays
+        Pawn[] bluePawns = new Pawn[4];
+        Pawn[] orangePawns = new Pawn[4];
+        Pawn[] yellowPawns = new Pawn[4];
+        Pawn[] greenPawns = new Pawn[4];
+        
+        // Populate pawn arrays, and place pawns on proper homes
+        for (int z = 0; z < homes.length; z++) {
+            int[][] teamHomes = homes[z];
+            Pawn[] currentPawns = null;
+
+            switch (z) {
+                case 0:
+                    currentPawns = bluePawns;
+                    break;
+                case 1:
+                    currentPawns = orangePawns;
+                    break;
+                case 2:
+                    currentPawns = yellowPawns;
+                    break;
+                case 3:
+                    currentPawns = greenPawns;
+                    break;
+            }
+
+            for (int i = 0; i < teamHomes.length; i++) {
+                int[] coords = teamHomes[i];
+                currentPawns[i] = new Pawn(spots[coords[1]][coords[0]], z+1, gc); 
+            }
+        }
+
+        
     }
 }
--- src/ludoGame/Pawn.java
+++ src/ludoGame/Pawn.java
@@ -1,8 +1,10 @@
 package ludoGame;
 
 import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Pawn {
+	private GraphicsContext gc;
 	private Spot spot; // Current spot of the pawn
 	private int player = 0;
 	private final int PAWN_NUM = 4; // Number of pawns on each team
@@ -14,9 +16,38 @@ public class Pawn {
 	private Image yellow = new Image("file:src/images/yellow.png");
 	private Image green = new Image("file:src/images/green.png");
 	
-	public Pawn(Spot spot, int player) {
+	public Pawn(Spot spot, int player, GraphicsContext gc) {
+		this.gc = gc;
 		this.spot = spot;
 		this.player = player;
+		Image img = null;
+		
+		switch (player) {
+			case 1:
+				img = blue;
+				break;
+			case 2:
+				img = orange;
+				break;
+			case 3:
+				img = yellow;
+				break;
+			case 4:
+				img = green;
+				break;
+		}
+		
+		gc.save(); // Save the current state of the graphics context
+	    gc.translate(spot.getX() + spot.getSize()/2, spot.getY() + spot.getSize()/2);
+	    
+	    double scale = 0.1;
+	    
+	    double scaledWidth = img.getWidth() * scale;
+	    double scaledHeight = img.getHeight() * scale;
+
+	    gc.drawImage(img, -scaledWidth / 2, -scaledHeight / 2, scaledWidth, scaledHeight); // Draw the scaled image
+
+	    gc.restore(); // Restore the saved state (removes the translation and rotation)
 	}
 	//it do
 	public boolean occupied(Spot spot) {
