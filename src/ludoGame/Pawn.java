22b1ae2537306bdae8b9a6290ee4a6e2eb2ed8e3
saleem <salee@10.220.17.236>
2024-04-06 18:05:07 -0400

    i do colors

--- src/ludoGame/Pawn.java
+++ src/ludoGame/Pawn.java
@@ -4,16 +4,37 @@ public class Pawn {
 	private int[] pos = new int[2];
 	private int player =0;
 	private int pawnnum = 4;
+	private String color;
 	
 	public Pawn(int[] spot, int player) {
 		this.pos= spot;
 		this.player=player;
+		switch (player) {
+		case 1:
+			this.color= "Blue";
+			break;
+		case 2:
+			this.color="Orange";
+			break;
+		case 3:
+			this.color="Yellow";
+			break;
+		case 4:
+			this.color="Green";
+			break;
+		}
 	}
 		
 	
 	public int[] returnPosition() {
 		return pos;
 	}
+	
+	public String pawnColor() {
+		return color;
+	}
+	
+
 	
 	
 	
