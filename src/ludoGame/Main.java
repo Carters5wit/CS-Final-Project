9e498a13f4e55ea23afa14ec44d607da6af7879e
Reggie Andrade <andrader6@wit.edu>
2024-04-09 12:27:08 -0400

    Added ludo.png and basic main menu logic

--- src/ludoGame/Main.java
+++ src/ludoGame/Main.java
@@ -2,91 +2,135 @@ package ludoGame;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
+    
+	public static <T extends Node> void addToParent(T node, StackPane r) {
+        r.getChildren().add(node);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-    	// Set title and create window frame root
-    	primaryStage.setTitle("Ludo Game");
-        final StackPane root = new StackPane();
-        
-        // Create a Board object and add it to the root
-        Board board = new Board(750, 750);
-        GraphicsContext gc = board.getGraphicsContext();
-        root.getChildren().add(board.getCanvas());
-        
-        // Create a Pane for pawns and overlay it on top of the board
-        Pane pawnPane = new Pane();
-        root.getChildren().add(pawnPane);
-        
-        // Set scene size and show to screen
-        Scene sc = new Scene(root, 750, 750);
-        primaryStage.setScene(sc);
-        primaryStage.show();
-        
-        
-        // Get spots on the board
-        Spot[][] spots = board.getSpots();
-        int[][][] homes = board.getHomes();
-        int[][] bluePath = board.getPath(0);
-        
-        // Initialize pawn arrays
-        Pawn[] bluePawns = new Pawn[4];
-        Pawn[] orangePawns = new Pawn[4];
-        Pawn[] yellowPawns = new Pawn[4];
-        Pawn[] greenPawns = new Pawn[4];
-        
-        // Populate pawn arrays, and place pawns on proper homes
-        for (int z = 0; z < homes.length; z++) {
+	
+	public static void populate(Spot[][] spots, int[][][] homes, Pawn[][] pawns, Pane p) {
+		for (int z = 0; z < homes.length; z++) {
             int[][] teamHomes = homes[z];
             Pawn[] currentPawns = null;
 
             switch (z) {
                 case 0:
-                    currentPawns = bluePawns;
+                    currentPawns = pawns[0];
                     break;
                 case 1:
-                    currentPawns = orangePawns;
+                    currentPawns = pawns[1];
                     break;
                 case 2:
-                    currentPawns = yellowPawns;
+                    currentPawns = pawns[2];
                     break;
                 case 3:
-                    currentPawns = greenPawns;
+                    currentPawns = pawns[3];
                     break;
             }
 
             for (int i = 0; i < teamHomes.length; i++) {
                 int[] coords = teamHomes[i];
-                currentPawns[i] = new Pawn(spots[coords[1]][coords[0]], z + 1, pawnPane);
+                currentPawns[i] = new Pawn(spots[coords[1]][coords[0]], z + 1, p);
             }
         }
-        
-        Pawn[][] pawns = {bluePawns, orangePawns, yellowPawns, greenPawns};
-        
-        // Set up event handler to detect clicks on pawns
-        for (Pawn[] teamPawns : pawns) {
+	}
+	
+	public static void clickConnectPawns(Pawn[][] pawns) {
+		for (Pawn[] teamPawns : pawns) {
         	for (int i = 0; i < teamPawns.length; i++) {
         		final int index = i; // Final variable capturing the current value of i
                 teamPawns[i].getImageView().setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent event) {
                         System.out.println("Pawn clicked! Player: " + teamPawns[index].pawnColor() + " | ID: " + (index+1));
-                        // TODO: Perform pawn click logic
+                        // TODO: Perform pawn click logic, remove debug print
                     }
                 });
             }
         }
+	}
+	
+	public static void createMenu(Stage primaryStage, Scene sc) {
+    	// Window setup
+		Stage menu = new Stage();
+    	menu.setTitle("Ludo Game");
+    	StackPane menuRoot = new StackPane();
+    	Scene menuScene = new Scene(menuRoot, 600, 350);
+    	menu.setScene(menuScene);
+    	menu.show();
+    	
+    	// Button creation
+    	Button playButton = new Button("Play Ludo!");
+    	addToParent(playButton, menuRoot); // Add button to menuRoot
+    	playButton.setTranslateX(-200); // Change X
+    	playButton.setTranslateY(-100); // Change Y
+    	playButton.setPrefSize(100, 50); // Change size (all buttons should be this size)
+    	
+    	// When play button is pressed...
+    	playButton.setOnAction(e -> {
+    		menu.close(); // Close the main menu
+    		primaryStage.setScene(sc); // Set the Ludo game scene
+    		primaryStage.show(); // Show the Ludo game stage
+    	});
+    	
+    	// TODO: Create exit button to close the main menu and exit the program
+    	// TODO: Add the ludo.png image to the right of the main menu
+    	// TODO: Create help button to show game instructions (depends if we have time)
+	}
+	
+    @Override
+    public void start(Stage primaryStage) {
+    	primaryStage.setTitle("Ludo"); // Set title if main game
+    	
+    	// Game Window variables
+        final StackPane root = new StackPane();
+        final Board board = new Board(750, 750);
+        final GraphicsContext gc = board.getGraphicsContext();
+        final Pane pawnPane = new Pane();
+        final Scene sc = new Scene(root, 750, 750);
         
+        // Board variables
+        Spot[][] spots = board.getSpots();
+        int[][][] homes = board.getHomes();
+        int[][] bluePath = board.getPath(1);
+        int[][] orangePath = board.getPath(2);
+        int[][] greenPath = board.getPath(3);
+        int[][] yellowPath = board.getPath(4);
+        
+        // Pawn variables
+        Pawn[] bluePawns = new Pawn[4];
+        Pawn[] orangePawns = new Pawn[4];
+        Pawn[] yellowPawns = new Pawn[4];
+        Pawn[] greenPawns = new Pawn[4];
+        Pawn[][] pawns = {bluePawns, orangePawns, yellowPawns, greenPawns};
+        
+        // Show the main menu
+        createMenu(primaryStage, sc);
+        
+        // Add board to StackPane
+        addToParent(board.getCanvas(), root);
+        
+        // Add pawn pane to StackPane
+        addToParent(pawnPane, root);
+        
+        // Populate pawn arrays (create and place pawns on home spots)
+        populate(spots, homes, pawns, pawnPane);
+        
+        // Set up event handler to detect clicks on pawns
+        clickConnectPawns(pawns);
+        
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
     }
 }
